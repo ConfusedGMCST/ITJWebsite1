2805fa2689aeb558ff8470abcc5cc6bd14929afe
ConfusedG <christian.haase@mcvts.org>
2024-09-25 20:13:15 -0400

    added animations
    >to page header
    >to 'Software Engineering' section

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+let executed = false;
+
+window.onload = function() {
+    const pageTitle = document.getElementById('pageTitle');
+    const SEHeader = document.getElementById('SEHeader');
+
+    pageTitle.style.width = '90%';
+
+    SEHeader.style.transition = '0.5s ease';
+    SEHeader.onmouseenter = function(){
+        SEHeader.style.color = 'lightblue';
+        SEHeader.style.width = '90%';
+    }
+    SEHeader.onmouseleave = function(){
+        SEHeader.style.color = 'white';
+        SEHeader.style.width = '80%';
+    }
+}
+
 function toGithub() {
     window.location.replace("https://github.com/ConfusedGMCST");
 }
